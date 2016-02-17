4e855581943a1a9a85ebf7382de5ff44ed3d1977
fylooi <fy.looi@gmail.com>
2016-02-17 23:32:09 +0800

    initial commit

---
+++ git.rb
@@ -0,0 +1,51 @@
+class Git
+  attr_accessor :calls
+  Mutt = Mutex.new
+ 
+  class << self
+    def singleton_git
+      Mutt.synchronize { @singleton_git ||= new }  
+    end
+
+    def singleton_git=(val)
+      Mutt.synchronize { @singleton_git = val }  
+    end
+
+    def git(*args)
+      self.singleton_git = nil
+
+      Thread.new do 
+        sleep(0.1)
+        singleton_git.invoke
+      end
+
+      singleton_git
+    end
+  end
+
+  def initialize
+    @calls = []
+  end
+
+
+  def method_missing(m, *args, &block)
+    self.class.singleton_git.tap do |git|
+      incoming_methods = [m] + args
+      git.calls << incoming_methods
+    end
+  end 
+
+  def to_s
+    "git #{calls.join(' ')}"
+  end
+
+  def inspect
+    to_s
+  end
+
+  def invoke
+    system(to_s)
+  end
+end
+
+define_method(:git) { Git.git }
